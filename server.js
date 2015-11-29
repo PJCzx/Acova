9da5c0c5bf5000b74de738946fbfa8542f93169a
Pierre-Julien Cazaux <cazaux.pierre@gmail.com>
2015-11-29 14:15:36 +0100

    installing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var GPIO_POS = 15;
 var GPIO_NEG = 16;
 var GPIO_PARKING = 7;
 
-var myHeatingSystem = new acova("Salon", GPIO_POS, GPIO_NEG, false).init();
+var myHeatingSystem = new acova("Salon", GPIO_POS, GPIO_NEG).init();
 var myParkingSystem = new parking(GPIO_PARKING, false);
 
 var rules = {};
