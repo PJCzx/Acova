0c570bb60d9ffeb9687d03c855d3a38697517e9a
Pierre-Julien Cazaux <cazaux.pierre@gmail.com>
2016-01-14 08:22:19 +0100

    Add node-dht-sensor

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ var schedule = require('node-schedule');
 var sys=require("sys");
 var exec=require("child_process").exec;
 
+var sensorLib = require('node-dht-sensor');
+
+var sensor = {
+    initialize: function () {
+        return sensorLib.initialize(22, 4);
+    },
+    read: function () {
+        var readout = sensorLib.read();
+        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+            'humidity: ' + readout.humidity.toFixed(2) + '%');
+        setTimeout(function () {
+            sensor.read();
+        }, 2000);
+    }
+};
+
+if (sensor.initialize()) {
+    sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
+
 var acova = require('./lib/acova.js');
 var parking = require('./lib/parking.js');
 
