4e865062475fd7a522218765b4f829b58ae79199
Pierre-Julien Cazaux <cazaux.pierre@gmail.com>
2016-01-15 20:46:27 +0100

    Chart

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,85 +1,50 @@
-/*var datachartist = {
-  // A labels array that can contain any sort of values
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  // Our series array that contains series objects or in this case series data arrays
-  series: [
-    [5]
-  ]
-};*/
-
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
-//var chartistGraph = new Chartist.Line('.ct-chart', datachartist);
-
-/*
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
-
-
-var temperatures = {
-    labels: ["Init"],
-    datasets: [
-        {
-            label: "Temperatures",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [19]
-        }
-    ]
-};
-
-var ctx = $("#temperature").get(0).getContext("2d");
-var temperatureChart = new Chart(ctx).Line(temperatures, {
-    bezierCurve: true
-});
-
-var ctx = $("#humidity").get(0).getContext("2d");
-var humidityChart = new Chart(ctx).Line(data, {
-    bezierCurve: true
-});
-
-
 $.ajax({
-	url: "/temperatures",
-}).success(function(data) {
-	for (var row of data) {
-		temperatures.labels.push(row.date);
+	url: "http://home.pierrejuliencazaux.com/sensordata",
+}).success(function(receivedData) {
+    var builtdata = {
+        labels: [],
+        datasets: [
+            {
+                label: "Temperatures",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: []
+            },
+            {
+                label: "Humidities",
+                fillColor: "rgba(0,220,220,0.0)",
+                strokeColor: "rgba(0,220,220,1)",
+                pointColor: "rgba(0,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: []
+            }
+        ]
+    };
 
-		//temperatures.datasets[0].data.push(row.value);
-		datachartist.series[0].push(row.value);
-	}
-	//temperatureChart.update();
+    for (var i = 0; i < receivedData.length; i++) {
+        builtdata.datasets[0].data.push(receivedData[i].temperature);
+        builtdata.datasets[1].data.push(receivedData[i].humidity);
+        if(i%6 == 0) {
+            var date = new Date(receivedData[i].date);
+            builtdata.labels.push(date.getHours());
+        } else {
+            builtdata.labels.push(" ");
+        }
+    }
+
+    var ctx = $("#chart").get(0).getContext("2d");
+    var temperatureChart = new Chart(ctx).Line(builtdata, {
+        bezierCurve: true
+    });
 
 });
-*/
+
 
 setInterval(function() {
 
